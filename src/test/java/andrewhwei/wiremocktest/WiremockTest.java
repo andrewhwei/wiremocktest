ded36bc50b67305091d3c5ba5c1591b104ac5995
Andrew Wei <Andrew_Wei@Andrew-Weis-MacBook-Pro.local>
2016-08-29 13:40:40 -0700

    All sample tests succeeding

--- src/test/java/andrewhwei/wiremocktest/WiremockTest.java
+++ src/test/java/andrewhwei/wiremocktest/WiremockTest.java
@@ -1,54 +1,55 @@
 package andrewhwei.wiremocktest;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+import static com.jayway.restassured.RestAssured.*;
 
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-
-import static com.jayway.restassured.RestAssured.*;
-
 public class WiremockTest {
 
-	public void setupStub() {	         
+	public void setupStub() {
 	    stubFor(get(urlEqualTo("/an/endpoint"))
 	            .willReturn(aResponse()
 	                .withHeader("Content-Type", "text/plain")
 	                .withStatus(200)
 	                .withBody("You've reached a valid WireMock endpoint")));
 	}
-	
+
+	// The false argument allows unmatched web requests to be served by 404 response.
 	@Rule
-	public WireMockRule wireMockRule = new WireMockRule(8090);
+	public WireMockRule wireMockRule = new WireMockRule(options().port(8090), false);
 	
 	@Test
 	public void testStatusCodePositive() {
 	    setupStub();
-	         
+
 	    given().
 	    when().
 	        get("http://localhost:8090/an/endpoint").
 	    then().
 	        assertThat().statusCode(200);
 	}
-	     
+
 	@Test
 	public void testStatusCodeNegative() {
 	    setupStub();
-	         
+
 	    given().
 	    when().
 	        get("http://localhost:8090/another/endpoint").
 	    then().
 	        assertThat().statusCode(404);
 	}
-	     
+
 	@Test
 	public void testResponseContents() {
 	    setupStub();
-	     
+
 	    String response = get("http://localhost:8090/an/endpoint").asString();
 	    Assert.assertEquals("You've reached a valid WireMock endpoint", response);
 	}
